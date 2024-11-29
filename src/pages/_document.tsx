bef8512305fcb6a835e2c03180f5a30216a44e3b
Jacob Varghese <jakestar18@gmail.com>
2024-11-29 18:57:53 +0530

    Responsiveness

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
         <link
           href="https://fonts.googleapis.com/css2?family=Londrina+Shadow&family=Playfair+Display:wght@400;600&family=Alex+Brush&family=Limelight&family=Lexend:wght@300;400;500&display=swap"
           rel="stylesheet"
